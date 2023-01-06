e317f93649efd1f4e6f51e4b9de1d593329d9a29
Ary Mertz <arzmertz@yahoo.com.br>
2023-01-05 23:18:35 -0300

    Segunda Solução - Aula 2

---
+++ 01-ComandosCondicionais/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01_ComandosCondicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 18;
+            double altura  = 1.60;
+
+            //&& (ou) (||)
+            if ((idade == 18) || (altura > 1.50))
+            {
+                if(altura > 1.50)
+                    Console.WriteLine("Pode entrar");
+            }
+
+            else
+            {
+                Console.WriteLine("ANão pode entrar");
+            }
+        }
+    }
+}
---
+++ 02-comandoRepeticao_While/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02_comandoRepeticao_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i <= 10)
+            {
+                Console.WriteLine($" {i} x 4 = {i * 4 }  ");
+                i++; // i = i+1
+            }
+            Console.WriteLine("---------------");
+
+            for (int j = 0; j <= 10; j++)
+            {
+                Console.WriteLine($" {j} x 6 =  {j * 6}");
+            }
+
+
+
+
+        }
+    }
+}
